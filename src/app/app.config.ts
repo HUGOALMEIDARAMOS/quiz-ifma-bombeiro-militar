a2a4719b80622477785fd88e570573547e0feffb
hugoalmeidaramos <hugo.undb@gmail.com>
2026-04-21 21:22:05 -0300

    feat:adicionar componente de header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,13 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+        floatLabel: 'auto'
+      }
+    }
   ],
 };
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,32 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-  // { path: '', component: Question },
-  // { path: 'result', component: Result }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then(m => m.Login)
+  },
+    {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register').then(m => m.Register)
+  },
+  {
+    path: '',
+    loadComponent: () => import('./pages/layout/main-layout/main-layout').then(m => m.MainLayout),
+    // canActivate: [authGuard], // Onde você colocaria sua proteção
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
